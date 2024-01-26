601a8737b4772e7be081f4cc23f9e3731f9ac65c
Tran Thanh Truc <tructt16@fe.edu.vn>
2024-01-26 15:55:35 +0700

    The source code to include a fibonaci.html where fibonnaci is calculated at server

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const querystring = require('querystring');
+const fb = require('fibo-lib');
+
 
 
 const data = fs.readFileSync(`${__dirname}/fibonnaci.html`,'utf-8');
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const parsedUrl = url.parse(req.url);
+    const pathName = parsedUrl.pathname;
+    console.log(pathName);
+
+    const query = querystring.parse(parsedUrl.query);
+
     if (pathName ==='/' || pathName ==='/overview'){
         res.end('This is the OVERVIEW');
     }else if (pathName ==='/product')
@@ -15,11 +23,25 @@ const server = http.createServer((req, res) => {
         }
     else if (pathName ==='/fibo')
         {
-            res.writeHead(200, {
-                'Content-type': 'text/html'
-              });
-            res.end(data);
-            console.log(data);
+            if (query.number) {
+                const n = parseInt(query.number);
+                const result = fb.fibo(n);
+
+                console.log(`client push button ${n}`);
+                
+                const dynamicData = `<p>Fibonacci sequence up to ${n}:</p><p>${result}</p>`;
+                //const modifiedData = data.replace('{{DYNAMIC_DATA}}', dynamicData);
+                //console.log(modifiedData);
+                res.writeHead(200, {'Content-type': 'text/html'});
+                res.end(dynamicData);
+                //res.end('page not found');
+
+            }
+            else {
+                res.writeHead(200, {'Content-type': 'text/html'});
+                res.end(data);
+            }
+            
         }
     else {
         res.writeHead(404, {'Content-type': 'text/html',
